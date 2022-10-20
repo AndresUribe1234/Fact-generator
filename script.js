b9e6cf56e1d7605f7bb2117cad488deb2e84916e
AndresUribe1234 <anduri1997@gmail.com>
2022-10-19 19:15:28 -0500

    accordion functionality working before refactoring

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const expandReduceIcon = document.querySelectorAll(".expand-icon");
+
+function expandReduce(e) {
+  const target = e.target;
+  const containerParent = target.closest(".question-container");
+  const answerToExpand = containerParent.querySelector(".answer");
+  const icon = containerParent.querySelector(".expand-icon");
+
+  if (answerToExpand.classList.contains("hidden")) {
+    answerToExpand.classList.remove("hidden");
+    icon.innerText = "-";
+  } else {
+    answerToExpand.classList.add("hidden");
+    icon.innerText = "+";
+  }
+}
+
+expandReduceIcon.forEach((ele) => ele.addEventListener("click", expandReduce));
